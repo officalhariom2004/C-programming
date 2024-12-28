5c215c56a6bf9a8121caa6c7cd33d5134e0b9c97
hari om <hariom.21242@gmail.com>
2024-12-28 21:47:22 +0530

    question

---
+++ Chapter 1/01_practic.c
@@ -0,0 +1,10 @@
+//write a c program to calculate area of a rectangle
+// using hard cord input
+#include <stdio.h>
+int main()
+{
+int length = 3, breath = 4;
+int area = length*breath;
+ printf("area of rectangle is %d" , area);
+ return 0;
+}
