bad51f40b239fe73e439cd3ea98680bb981b67fa
hari om <hariom.21242@gmail.com>
2024-12-29 22:55:25 +0530

    simple interest

---
+++ Chapter 1/simpleintrest.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int principle = 100;
+    int rate = 5;
+    int time = 3;
+    int simpleinterest =(principle * rate * time);
+    printf("the value of simple intesr is %d" ,simpleinterest);
+    return 0;
+}
