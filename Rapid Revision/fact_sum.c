d781fd93b423c47def36aa80539a7d2e57eb2bdb
hari om <hariom.21242@gmail.com>
2025-01-01 21:26:36 +0530

    factori

--- Rapid Revision/fact_sum.c
+++ Rapid Revision/fact_sum.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+int fact(int);
 int main(){
-int n;
-    
+int n,sum=0;
+    printf("Enter the term : ");
+    scanf("%d",&n);
+    for (int i = 1; i <=n; i++)
+    {
+        sum = sum +fact(i);
+    }
+    printf("Sum of factoril = %d",sum);
     return 0;
+}
+int fact(int n){
+    int facts=1;
+    for (int i = 1; i <=n; i++)
+    {
+        facts = facts*i;
+    }
+    return facts;
 }
